3863c942cd20db4248076277095942fac67fb609
huntznavarro <huntzorravan@gmail.com>
2025-11-18 08:10:13 +0800

    added arithmetic funcrtion

---
+++ math.js
@@ -0,0 +1,26 @@
+function add(num1,num2){
+    sum = num1 + num2
+    console.log(sum);
+}
+ console.log (add(10,9));
+
+ 
+function diff(num1,num2){
+
+    diff = num1 - num2
+    console.log(diff);
+}
+    console.log (diff(10,9));
+
+function prod(num1,num2){
+
+    prod = num1*num2
+    console.log(prod);  
+}
+      console.log(prod(10,9));
+
+function quo(num1,num2){
+    quo = num1 / num2
+    console.log(quo);   
+}
+     console.log(quo(10,9));
