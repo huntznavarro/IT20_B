93cc687b55c83ce4b5251ac5f6372f768f66f8eb
huntznavarro <huntzorravan@gmail.com>
2025-11-25 08:20:32 +0800

    added insert and search  method

---
+++ array.js
@@ -0,0 +1,80 @@
+class array{
+    constructor(initialData =[]){
+        this.data = {};
+        this.length = 0;
+
+         for(let i=0; i<initialData.length; i++){
+
+            this.data[i] = initializedData[i];
+            this.length++;
+         }
+
+    }
+    traverse(){
+        let output = "";
+
+        for(let i =0; i<this.length; i++){
+            output += this.data[i] + "";
+        }
+            console.log(output);
+    }
+
+   insert(index,element){
+        //Correcting statement
+        if(index < 0  || index > this.length){
+            console.log("Invalid index");
+            return;
+        }
+
+        //Shifting condition
+        for(let i = this.length; i > index; i--){
+            this.data[i] = this.data[i-1];
+        }
+
+        this.data[index] = element;
+        this.length++;
+
+    }
+
+    search(element){
+        for(let i=0; i < this.length;i++){
+            if(this.data[i] === element){
+                console.log(element + " is found at index "+ i);
+                return i;
+            }
+        }
+        
+        console.log(element + " is not found at any index ");
+        return -1;
+    }
+
+    update(index,element){
+
+        //Correcting statement
+        if(index < 0 || index >= this.length){
+            console.log("Invalid Index");
+        }
+
+        this.data[index] = element;
+    }
+
+
+
+
+
+
+
+
+// 
+    arr1 = new Array([4,2,3]);
+
+     arr1.traverse();
+
+
+
+    
+
+
+
+    
+}
